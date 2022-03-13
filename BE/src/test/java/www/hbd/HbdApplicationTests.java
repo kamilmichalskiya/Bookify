05f2678ce969e72b5ca29d75cb6d4ff68a20625c
kamilmichalskiya <kamilmichalskiya@users.noreply.github.com>
2022-03-13 09:23:24 +0100

    HBP-12 init sprig boot app

---
+++ BE/src/test/java/www/hbd/HbdApplicationTests.java
@@ -0,0 +1,13 @@
+package www.hbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HbdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
