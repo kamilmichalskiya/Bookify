6194b714c5d723fbae29fb73157299af77f656bf
kamilmichalskiya <kamilmichalskiya@users.noreply.github.com>
2022-03-26 15:19:58 +0100

    HBP-1 add new structure of application

--- app/src/test/java/pl/inf/app/AppApplicationTests.java
+++ app/src/test/java/pl/inf/app/AppApplicationTests.java
@@ -1,13 +1,13 @@
-package www.hbd;
+package pl.inf.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HbdApplicationTests {
+class AppApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
