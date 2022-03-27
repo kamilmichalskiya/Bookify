771f462f7eb235393bd8310d2c74219ffc50430f
kamilmichalskiya <kamilmichalskiya@users.noreply.github.com>
2022-03-27 09:02:52 +0200

    HBP-10 Add Message broker with simple websocket queue

--- app/src/main/java/pl/inf/app/testConnection/TestController.java
+++ app/src/main/java/pl/inf/app/testConnection/TestController.java
@@ -1,5 +1,6 @@
 package pl.inf.app.testConnection;
 
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("hello")
 public class TestController {
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public TestController(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
+
     @GetMapping
     public String hello() {
+        messagingTemplate.convertAndSend("/topic/test", "websocket test");
         return "hello";
     }
 }
