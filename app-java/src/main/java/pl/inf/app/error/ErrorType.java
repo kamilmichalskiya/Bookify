431390ecf755e91c0ffd25abbab690d6574ebcaa
kamilmichalskiya <kamilmichalskiya@users.noreply.github.com>
2022-06-21 23:46:06 +0200

    HBP-57 | Format error msg

--- app-java/src/main/java/pl/inf/app/error/ErrorType.java
+++ app-java/src/main/java/pl/inf/app/error/ErrorType.java
@@ -13,23 +13,22 @@ import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorType {
-    ROOM_NOT_FOUND("B001", "Room not found. Room id: {}", NOT_FOUND),
-    ROOM_ADDING_ERROR("B002", "Room cannot be added. Room : {}", UNPROCESSABLE_ENTITY),
-    ROOM_UPDATING_ERROR("B003", "Room cannot be updated. Room : {}", UNPROCESSABLE_ENTITY),
+    ROOM_NOT_FOUND("B001 | Room not found. Room id: {}", NOT_FOUND),
+    ROOM_ADDING_ERROR("B002 | Room cannot be added. Room : {}", UNPROCESSABLE_ENTITY),
+    ROOM_UPDATING_ERROR("B003 | Room cannot be updated. Room : {}", UNPROCESSABLE_ENTITY),
 
-    EMPLOYEE_NOT_FOUND("B004", "Employee not found. Employee id: {}", NOT_FOUND),
-    EMPLOYEE_ADDING_ERROR("B005", "Employee cannot be added. Employee : {}", UNPROCESSABLE_ENTITY),
-    EMPLOYEE_UPDATING_ERROR("B006", "Employee cannot be updated. Employee : {}", UNPROCESSABLE_ENTITY),
+    EMPLOYEE_NOT_FOUND("B004 | Employee not found. Employee id: {}", NOT_FOUND),
+    EMPLOYEE_ADDING_ERROR("B005 | Employee cannot be added. Employee : {}", UNPROCESSABLE_ENTITY),
+    EMPLOYEE_UPDATING_ERROR("B006 | Employee cannot be updated. Employee : {}", UNPROCESSABLE_ENTITY),
 
-    OFFER_NOT_FOUND("B007", "Offer not found. Offer id: {}", NOT_FOUND),
-    OFFER_ADDING_ERROR("B008", "Offer cannot be added. Offer : {}", UNPROCESSABLE_ENTITY),
-    OFFER_UPDATING_ERROR("B009", "Offer cannot be updated. Offer : {}", UNPROCESSABLE_ENTITY),
+    OFFER_NOT_FOUND("B007 | Offer not found. Offer id: {}", NOT_FOUND),
+    OFFER_ADDING_ERROR("B008 | Offer cannot be added. Offer : {}", UNPROCESSABLE_ENTITY),
+    OFFER_UPDATING_ERROR("B009 | Offer cannot be updated. Offer : {}", UNPROCESSABLE_ENTITY),
 
-    INVALID_SEARCH_PARAMS("B010", "Search params are invalid. Search params : {}", UNPROCESSABLE_ENTITY),
-    RESERVATION_CREATING_ERROR("B011", "Reservation cannot be created. Reservation : {}", UNPROCESSABLE_ENTITY),
-    RESERVATION_VALIDATION_ERROR("B012", "Reservation is not valid. Reservation : {} | UiReservation : {}", UNPROCESSABLE_ENTITY);
+    INVALID_SEARCH_PARAMS("B010 | Search params are invalid. Search params : {}", UNPROCESSABLE_ENTITY),
+    RESERVATION_CREATING_ERROR("B011 | Reservation cannot be created. Reservation : {}", UNPROCESSABLE_ENTITY),
+    RESERVATION_VALIDATION_ERROR("B012 | Reservation is not valid. Reservation : {} | UiReservation : {}", UNPROCESSABLE_ENTITY);
 
-    private final String code;
     private final String description;
     private final HttpStatus defaultStatus;
 }
--- app-java/src/main/java/pl/inf/app/error/ProcessException.java
+++ app-java/src/main/java/pl/inf/app/error/ProcessException.java
@@ -3,12 +3,14 @@ package pl.inf.app.error;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+import java.util.Arrays;
+
 /**
  * Custom exception to convert to user response.
  */
 @Getter
 public class ProcessException extends RuntimeException {
-    private final ErrorType errorType;
+    private final String description;
     private final HttpStatus httpStatus;
     private final Object[] params;
 
@@ -19,10 +21,16 @@ public class ProcessException extends RuntimeException {
      * @param params    arguments to fill descriptions
      */
     public ProcessException(final ErrorType errorType, final Object... params) {
-        super(errorType.getCode());
+        super(errorType.getDescription());
         this.httpStatus = errorType.getDefaultStatus();
-        this.errorType = errorType;
+        this.description = errorType.getDescription();
         this.params = params;
     }
 
+    @Override
+    public String toString() {
+
+        return "ProcessException: " + "httpStatus=" + httpStatus + ", description=" + description + ", params=" + Arrays.toString(
+                params);
+    }
 }
